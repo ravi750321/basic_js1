a69e2ea6523ea60b80d19ad114c6b29707815498
ravi750321 <ravistotalgaming@gmail.com>
2025-02-02 06:01:17 +0000

    all done till object 2

--- 02basic/02Object2.js
+++ 02basic/02Object2.js
@@ -1,14 +1,64 @@
-// const user1 = new Object()   // Singleton Method
-
+// const user1 = new Object()
 // console.log(user1);
 
-const user2 = {
+const user1 = {
 
 }
 
-user2.id = 5455654
-user2.name = "Ravi"
-user2.city = "raipur"
-user2.isLoggedIn = false
+// console.log(user1);
 
-console.log(user2);
+user1.id = "12313"
+user1.name = "Ravi"
+user1.isLoggedIn = false
+// console.log(user1);
+
+// --------------------------------------------------------
+
+const regularUser = {
+    email:"ravi@google.com",
+    name:{
+        fullname:{
+            firstname:"ravi",
+            lname:"Sinha"
+        }
+    }
+}
+
+// console.log(regularUser.name.fullname.firstname);  // name? for data fetch
+
+// ****************************************
+
+obj1 ={ 1:"r", 2:"a"}
+obj2 ={ 3:"v", 4:"i"}
+
+// const obj3 = {obj1,obj2}
+
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1,...obj2}   // Spread Operator andstore in Object
+// console.log(obj3);
+
+const users = [
+    {
+        id:"485485",
+        uname:"ravi"
+    },
+    {
+        id:"485485",
+        uname:"ravi"
+    },
+    {
+        id:"485485",
+        uname:"ravi"
+    }
+]
+
+// console.log(user1);
+
+// console.log(Object.keys(user1));   // o/p keys of object
+
+// console.log(Object.values(user1));  // o/p values of object
+
+// console.log(Object.entries(user1));   // o/p entries of object
+  
+console.log(user1.hasOwnProperty("isLoggedIn"));  // check for object me property hai ya nhi
