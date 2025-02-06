8ccf55d7bc06630d0494241a12ea758bc2ded578
ravi750321 <ravistotalgaming@gmail.com>
2025-02-06 15:40:03 +0000

    alll done till truthy condition

--- 04Control_Flow/03truthy.js
+++ 04Control_Flow/03truthy.js
@@ -1,3 +1,50 @@
 //    
 
-const userEmail = ""
+// const userEmail = ""
+// const userEmail = "r@google.com"
+// const userEmail = []
+
+// if(userEmail){
+//     console.log("Got User Email");
+    
+// }else{
+//     console.log("Don't have user email");
+    
+// }
+
+// ============= falsy Value ==============
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// ============= truthy Value ==============
+// "0", "false", " ", [],{}, function(){} // empty function,
+
+// if(userEmail.length ===0){
+//     console.log("Array is Empty");
+    
+// }
+
+// const emptyObj = {}
+
+// if(Object.keys(emptyObj).length===0){
+//      console.log("Object is Empty");
+     
+// }
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+// =========== Terniary Operator ==========
+
+// condition ?? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80"): console.log("more than 80");
+
+
+
