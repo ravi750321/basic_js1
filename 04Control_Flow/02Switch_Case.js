b73debf36e9f8d7ed30c5155b1fd372336b4b913
ravi750321 <ravistotalgaming@gmail.com>
2025-02-06 13:54:09 +0000

    continue truthy

---
+++ 04Control_Flow/02Switch_Case.js
@@ -0,0 +1,55 @@
+//  =============  Switch Case Statement  ================
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//     case 2:
+//         console.log("Fab");
+        
+//         break;
+//     case 3:
+//         console.log("Mar");
+        
+//         break;
+//     case 4:
+//         console.log("Apr");
+        
+//         break;
+
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+// =============== In Case of String ==============
+const month = "fab"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        break;
+    case "fab":
+        console.log("Fab");
+        
+        break;
+    case "march":
+        console.log("Mar");
+        
+        break;
+    case "April":
+        console.log("Apr");
+        
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
